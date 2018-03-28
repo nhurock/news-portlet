dfa106b41096ee0b975b564a86e2f515cbaaee09
pushkinser <pushkinsergey.voronezh@gmail.com>
2018-03-28 14:00:01 +0300

    Добавил логгирование в JournalArticleContentSAXMap

--- src/main/java/ru/news/mapper/JournalArticleContentSAXMap.java
+++ src/main/java/ru/news/mapper/JournalArticleContentSAXMap.java
@@ -1,5 +1,7 @@
 package ru.news.mapper;
 
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.xml.Document;
 import com.liferay.portal.kernel.xml.DocumentException;
 import com.liferay.portal.kernel.xml.Node;
@@ -8,13 +10,14 @@ import com.liferay.portal.kernel.xml.SAXReaderUtil;
 public class JournalArticleContentSAXMap {
 
     private static final String ROOT_STATIC_CONTENT = "/root/static-content";
+    private static final Log log = LogFactoryUtil.getLog(JournalArticleContentSAXMap.class);
 
     public static String getContent(String journalArticleContent) {
         Document document = null;
         try {
             document = SAXReaderUtil.read(journalArticleContent);
         } catch (DocumentException e) {
-            e.printStackTrace();
+            log.error(e);
         }
 
         if (document == null) return null;
